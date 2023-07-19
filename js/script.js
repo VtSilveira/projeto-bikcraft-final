a0074990a366d58b8d74e520c18138ce6fa510b8
Vitor Silveira <vitor.silveiravtss@gmail.com>
2023-07-19 09:31:09 -0300

    all formated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 /* deixa o efeito de hover sempre ativo quando na página selecionada ------------- */
-const links = document.querySelectorAll('.header-menu a');
+const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
   const url = location.href; // pega a url atual
   const href = link.href; // o link atual do forEach
 
-  if (url.includes(href)) { // se forem iguais, i.e. está na página selecionada
+  if (url.includes(href)) {
+    // se forem iguais, i.e. está na página selecionada
     link.classList.add("ativo"); // adiciona a classe 'ativo' para a tag <a>
   }
 }
@@ -16,7 +17,7 @@ links.forEach(ativarLink); // chamada da funcao para cada elemento '.header-menu
 const parametros = new URLSearchParams(location.search); // pega os parametros da url
 
 function ativarProduto(parametro) {
-  const elemento = document.getElementById(parametro); // como o parametro é igual ao id, é fácil usar o getElementById 
+  const elemento = document.getElementById(parametro); // como o parametro é igual ao id, é fácil usar o getElementById
   if (elemento) {
     elemento.checked = true;
   }
@@ -25,24 +26,24 @@ function ativarProduto(parametro) {
 parametros.forEach(ativarProduto);
 /* ------------------------------------------------------------------------------- */
 /* perguntas frequentes ---------------------------------------------------------- */
-const perguntas = document.querySelectorAll('.perguntas button');
+const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
   const pergunta = event.currentTarget;
   const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
-  pergunta.setAttribute('aria-expanded', resposta.classList.toggle("ativa"));
+  pergunta.setAttribute("aria-expanded", resposta.classList.toggle("ativa"));
 }
 
 function eventosPerguntas(pergunta) {
-  pergunta.addEventListener('click', ativarPergunta);
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
 /* ------------------------------------------------------------------------------- */
 /* galeria de imagens ------------------------------------------------------------ */
 const galeria = document.querySelectorAll(".bicicleta-imagens img");
-const galeriaContainer = document.querySelector(".bicicleta-imagens")
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
 function trocarImagem(event) {
   const img = event.currentTarget;
@@ -50,7 +51,7 @@ function trocarImagem(event) {
 }
 
 function eventosGaleria(img) {
-  img.addEventListener('click', trocarImagem);
+  img.addEventListener("click", trocarImagem);
 }
 
 galeria.forEach(eventosGaleria);
